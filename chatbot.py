a767a9f3ba679059d1661fc153f900c44e635d7c
Asuna <2729685045@qq.com>
2023-11-21 20:10:33 +0800

    新增接口run，修复了一些bug

--- chatbot.py
+++ chatbot.py
@@ -2,13 +2,17 @@ from analyze_question import AnalysisQuestion
 from get_answer import Get_answer
 import gradio as gr
 
-if __name__ == "__main__":
+def run(question):
     aq = AnalysisQuestion()
     ga = Get_answer()
+    index, params , temp = aq.analysis_question(question)
+    answers = ga.get_data(index, params)
+    return answers
+
+if __name__ == "__main__":
     while True:
         question = input('请输入你想查询的信息：') 
-        index, params = aq.analysis_question(question)
-        answers = ga.get_data(index, params)
+        answers = run(question)
         print('答案:')
         for ans in answers:
             print(ans)
